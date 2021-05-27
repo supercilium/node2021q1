bcf9e2d15a0f88262498d14c7d6d8ec66f16648e
marusel <marusel.12@gmail.com>
2021-05-27 23:31:57 +0300

    change header

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -3,10 +3,10 @@ import { Request, Response, NextFunction } from 'express';
 import { logger } from '../../configs';
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers['x-access-token'];
+  const token = req.headers.authorization;
 
-  if (token && typeof token === 'string') {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, _decoded) => {
+  if (token) {
+    jwt.verify(token.replace('Bearer ', ''), process.env.ACCESS_TOKEN_SECRET, (err, _decoded) => {
       if (err) {
         logger.error('403 - Forbidden Error', err);
         res.status(403).send('Forbidden Error');
